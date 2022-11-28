7e5a388edccd87d4fb9ebf80032bcb4686c03649
Viktor Furieri <vkfurieri21@gmail.com>
2022-11-28 15:30:29 -0300

    add-env-done

--- connection.js
+++ connection.js
@@ -1,9 +1,5 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-let username = process.env.USERNAME;
-let password = process.env.PASSWORD;
-
-
 
 const connection = new Sequelize(process.env.DATABASE,process.env.USERNAME_DB,process.env.PASSWORD_DB,{
     host:'us-cdbr-east-06.cleardb.net',
