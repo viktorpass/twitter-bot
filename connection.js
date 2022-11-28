1a635e281dd2bf0275579e3cfa08bd2efb83d9a5
Viktor Furieri <vkfurieri21@gmail.com>
2022-11-28 15:27:30 -0300

    add_env

--- connection.js
+++ connection.js
@@ -5,7 +5,7 @@ let password = process.env.PASSWORD;
 
 
 
-const connection = new Sequelize(process.env.DATABASE,process.env.USERNAME,process.env.PASSWORD,{
+const connection = new Sequelize(process.env.DATABASE,process.env.USERNAME_DB,process.env.PASSWORD_DB,{
     host:'us-cdbr-east-06.cleardb.net',
     dialect:'mysql'
 
